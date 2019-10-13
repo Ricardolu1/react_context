22e06ba4ef8e50f6e42ecce22644bc092d6e3c48
Ricardo <568082704@qq.com>
2019-10-13 11:44:18 +0800

    context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      n:99
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <F1  n1={this.state.n}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+function F1(props) {
+  return (
+    <div> 
+      1111,{props.n1}
+      <F2  n2={props.n1}/>
+    </div>
+  )
+}
+function F2(props) {
+  return (
+    <div>
+      2222,{props.n2}
+      <F3 n3={props.n2}/>
+    </div>
+  )
+}
+function F3(props) {
+  return (
+    <div>
+      3333,{props.n3}
+      <F4 n4={props.n3}/>
+    </div>
+  )
+}
+function F4(props) {
+  return (
+    <div>
+      4444,{props.n4  }
+    </div>
+  )
+}
+
+export default App
